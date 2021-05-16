7019d93191f726572b1b8166a17e02fe8f71740e
Horace <hiuhongyung@gmail.com>
2021-05-17 01:52:01 +0800

    First commit (Header Nav Bar and next-auth)

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ["links.papareact.com", "platform-lookaside.fbsbx.com", "firebasestorage.googleapis.com"],
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+import { getSession } from "next-auth/client";
+export default function Home({ session }) {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main></main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
+}
